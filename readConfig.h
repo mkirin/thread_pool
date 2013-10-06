cd1169b7f9473f19f821fedb7b3b468fcf0fff39
mkirin <lingfengtengfei@163.com>
2013-10-06 23:45:29 +0800

    Create readConfig.h
    读取配置文件的头文件

---
+++ readConfig.h
@@ -0,0 +1,23 @@
+/***************************************************
+ *                                    
+ *            C语言读取配置文件                   
+ *                                                
+ * 1、 通过 char *GetParamValue(char *cpConfFile, 
+ *			        char *cpParam, char *cpValue) 
+ *     函数获取指定配置文件中的指定参数的值，保存 
+ *     在cpValue中                                
+ * 2、 配置文件风格说明：                         
+ *     (1) 自动忽略掉每行开头的空格               
+ *     (2) 以 # 开头的行是注释行                  
+ *     (3) 不能以 = 开头                          
+ *     (4) 支持行末注释                           
+***************************************************/
+#ifndef _READCONFIG_HEAD_
+#define _READCONFIG_HEAD_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char *GetParamValue(char *cpConfFile, char *cpParam, char *cpValue);
+
+#endif
